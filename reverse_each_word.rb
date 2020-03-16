187ae760767decbb4ccfe501c39c43e9c987a8cd
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 02:38:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
 sentence1 = []
- sentence1.map do |word| puts (word.split(" ").reverse).join(" ").reverse end
+ sentence1.map do |word| (word.split(" ").reverse).join(" ").reverse end
 
 end
